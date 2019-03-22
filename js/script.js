5ee1ebb6e357f0566e632133c93a992bb59f8418
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-22 13:38:34 +0500

    mading 13 lesson

--- js/script.js
+++ js/script.js
@@ -190,11 +190,61 @@ window.addEventListener('DOMContentLoaded', () => {
   
   inputPhone.forEach((elem) => {
     elem.addEventListener('focus', () => {
-      if(!/^\+\d*$/.test(elem.value)) elem.value = '+';
+      if(!/^\+\d*$/.test(elem.value)) {
+        elem.value = '+';
+      }
     });
     elem.addEventListener('keydown', (e) => {
-      if(!/\d/.test(e.key)) e.preventDefault();
+      if(!/\d/.test(e.key)) {
+        e.preventDefault();
+      }
     });
-});
+  });
+
+  //slider
+
+  let slideIndex = 1,
+    slides = document.querySelectorAll('.slider-item'),
+    prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.querySelectorAll('dot');
+
+  showSlides(slideIndex);
+
+  function showSlides (n) {
+
+    if(n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if(n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => item.style.display = 'none');
+    dots.forEach((item) => item.classList.remove('dot-active'));
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add = 'dot-active';
+    
+  }
+
+  function plusSlides (n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener('click', () => {
+    plusSlides(1);
+  });
+
 
 });
